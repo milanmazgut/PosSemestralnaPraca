6bd275cf4ebc70ab5fb414a3e054c92848fcff6b
milanmazgut <mazgut14@stud.uniza.sk>
2025-01-06 22:48:48 +0100

    communication between server and client using pipes

--- comunicationProcesses/client.c
+++ comunicationProcesses/client.c
@@ -1,19 +1,117 @@
-#include <stdio.h>
+#include "client.h"
 #include "pipe.h"
-#include <unistd.h>
-#include <string.h>
 
-int main(void) {
-    int fd = pipe_open_write("server_fifo");
-    char line[128];
-    while(1) {
-        printf("Type something: ");
-        fflush(stdout);
-        if(!fgets(line, sizeof(line), stdin)) {
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <errno.h>
+
+#define BUFFER_SIZE 256
+#define SERVER_PIPE "server_pipe"
+
+
+typedef struct {
+    char name[BUFFER_SIZE];
+    char pipePath[BUFFER_SIZE];
+    int  pipeFd;
+    int  serverFd;
+} ClientData;
+
+
+static void* reader_thread(void* arg) {
+    ClientData *cd = (ClientData*) arg;
+
+    while (1) {
+        char buf[BUFFER_SIZE];
+        memset(buf, 0, sizeof(buf));
+        size_t n = read(cd->pipeFd, buf, sizeof(buf) - 1);
+        if (n > 0) {
+            buf[n] = '\0';
+            printf("%s", buf);
+            if (buf[n - 1] != '\n') {
+                printf("\n");
+            }
+            fflush(stdout);
+        } 
+        else if (n == 0) {
+            printf("[CLIENT %s] Server closed my pipe => end.\n", cd->name);
+            fflush(stdout);
+            break;
+        } 
+        else {
+            perror("[CLIENT read]");
             break;
         }
-        // odosleme
-        write(fd, line, strlen(line));
     }
+    return NULL;
+}
+
+
+int client_main(const char *clientName) {
+    ClientData cd;
+    memset(&cd, 0, sizeof(cd));
+
+    strncpy(cd.name, clientName, BUFFER_SIZE - 1);
+    cd.name[BUFFER_SIZE - 1] = '\0';
+
+    snprintf(cd.pipePath, sizeof(cd.pipePath), "client_%s", cd.name);
+
+    printf("[CLIENT] Creating pipe '%s'\n", cd.pipePath);
+    pipe_init(cd.pipePath);
+
+    printf("[CLIENT %s] Opening server PIPE '%s' for writing...\n", cd.name, SERVER_PIPE);
+    cd.serverFd = pipe_open_write(SERVER_PIPE);
+
+    printf("[CLIENT %s] Opening '%s' \n", cd.name, cd.pipePath);
+    cd.pipeFd = pipe_open_read_write(cd.pipePath);
+    if (cd.pipeFd < 0) {
+        perror("[CLIENT] open");
+        return 2;
+    }
+
+    pthread_t tid;
+    if (pthread_create(&tid, NULL, reader_thread, &cd) != 0) {
+        perror("[CLIENT] pthread_create");
+        return 3;
+    }
+
+    printf("[CLIENT %s] Commands: roll, trade, end, shutdown, quit\n", cd.name);
+    fflush(stdout);
+
+    while (1) {
+        printf("[CLIENT %s] > ", cd.name);
+        fflush(stdout);
+
+        char line[BUFFER_SIZE];
+        if (!fgets(line, sizeof(line), stdin)) {
+            printf("[CLIENT %s] Stdin closed => exit.\n", cd.name);
+            break;
+        }
+        line[strcspn(line, "\n")] = '\0';
+
+        if (strcmp(line, "quit") == 0) {
+            char msg[BUFFER_SIZE];
+            snprintf(msg, sizeof(msg), "%s quit", cd.name);
+            write(cd.serverFd, msg, strlen(msg));
+
+            printf("[CLIENT %s] Quitting.\n", cd.name);
+            break;
+        }
+
+        char msg[BUFFER_SIZE];
+        snprintf(msg, sizeof(msg), "%s %s", cd.name, line);
+        write(cd.serverFd, msg, strlen(msg));
+    }
+
+    pipe_close(cd.pipeFd);
+    pipe_close(cd.serverFd);
+
+    pthread_join(tid, NULL);
+
+    pipe_destroy(cd.pipePath);
+
+    printf("[CLIENT %s] Exited.\n", cd.name);
     return 0;
 }
---
+++ comunicationProcesses/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "server.h"
+#include "client.h"
+
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2) {
+        printf("Usage:\n");
+        printf("  %s server\n", argv[0]);
+        printf("  %s client <Name>\n", argv[0]);
+        return 1;
+    }
+
+    if (strcmp(argv[1], "server") == 0) {
+        return server_main();
+    }
+    else if (strcmp(argv[1], "client") == 0) {
+        if (argc < 3) {
+            fprintf(stderr, "Missing client name.\n");
+            return 1;
+        }
+        return client_main(argv[2]);
+    }
+    else {
+        printf("Unknown mode '%s'. Use 'server' or 'client'.\n", argv[1]);
+        return 1;
+    }
+}
